dd928a9cdf636479980628d1991f776ee35ddcf2
Yara Castro <yaracastro24@gmail.com>
2019-08-01 21:23:23 -0500

    Initial commit axios working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+// import NasaCards from './NasaCard.js';
+
 
 function App() {
+
+  useEffect(() => {
+    axios
+    .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    .then(response => {
+      console.log(response)
+      // const nasa = response.data
+    })
+  },[])
   return (
     <div className="App">
       <p>
