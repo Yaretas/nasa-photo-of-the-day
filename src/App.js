77c86368ae16a51c1f808b5a86ad6a44fb41ac5d
Yara Castro <yaracastro24@gmail.com>
2019-08-03 11:28:46 -0500

    fixing typo on NasaCard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import "./App.css";
 import axios from "axios";
-import NasaCards from './NasaCards';
+import NasaCards from "./NasaCards";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -20,15 +20,17 @@ function App() {
     .catch(err => {
       console.log(`Err`);
     })
-  })
+  }, []);
   
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
+        <p>
+          Read through the instructions in the README.md file to build your NASA
+          app! Have fun 🚀!
+          </p>
+      <NasaCards imgUrl={setImages} />
+      </div>
+
   );
 }
 
--- src/Components/NadaCards.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NasaCards({imgUrl}) {
-    return(
-        <div style={{maxWidth: "100px"}}>
-            <img src={imgUrl} />
-        </div>
-    );
-}
